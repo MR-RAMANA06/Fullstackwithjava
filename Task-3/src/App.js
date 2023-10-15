f31dde616674e4731c7feaf887196e1750028730
MR_RAMANA_PCDY <128059803+MR-RAMANA06@users.noreply.github.com>
2023-10-15 09:52:02 +0530

    Add files via upload

---
+++ Task-3/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './index.html';
+import Calendar from "./calendar/Calendar";
+
+class App extends Component {
+  render() {
+    return (
+        <Calendar/>
+    );
+  }
+}
+
+export default App;
